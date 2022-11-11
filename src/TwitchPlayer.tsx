b718e584a25d1a1448459e12aa6f68602465b8a1
Taloth Saldono <Taloth@users.noreply.github.com>
2022-11-11 22:16:26 +0100

    Dynamic twitch player for testing purposes

---
+++ src/TwitchPlayer.tsx
@@ -0,0 +1,19 @@
+
+import {
+  Component,
+  ComponentProps,
+} from "solid-js";
+
+export const TwitchPlayer: Component<ComponentProps<"iframe">& { channel: string }> = (props) => {
+    return (
+      <iframe
+      src={`https://player.twitch.tv/?channel=${props.channel}&parent=${window.location.host}`}
+      height="100%"
+      width="100%"
+      allowfullscreen
+      class="w-screen h-screen absolute inset-0 z-10 pointer-events-none"
+    >
+    </iframe>
+    );
+  };
+  

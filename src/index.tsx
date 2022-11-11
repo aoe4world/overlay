d40ff02a4875c2ce12d346b62c8e310f95d1a3f6
Taloth Saldono <Taloth@users.noreply.github.com>
2022-11-11 22:14:56 +0100

    Use explicit registerSW to allow auto-reload after update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 /* @refresh reload */
 import './index.css';
 import { render } from 'solid-js/web';
+import { registerSW } from 'virtual:pwa-register';
 
 import App from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
+
+// Registers the serviceworker and will autoUpdate it on page load, then if there's an update refresh the page.
+registerSW({ immediate: true });
